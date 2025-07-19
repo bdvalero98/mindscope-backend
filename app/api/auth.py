65b67cffea82d660d5d241b3fb847d69af9ecd17
bdvalero98 <danilovalero25@gmail.com>
2025-07-19 11:25:18 -0500

    Crea endpoints de registro y login con JWT

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from app.auth.security import hash_password
-from app.auth.auth import create_access_token, authenticate_user
-from app.schemas.user import UserCreate, UserLogin, Token
 from app.db.database import get_db
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.auth.auth import create_access_token, authenticate_user
+from app.auth.security import hash_password
 from app.models.user import User
+from app.schemas.user import UserCreate, UserLogin, Token
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
